14b7cf081563c82b871109b85831e116c23d15d4
ceshantanu <shantanu.mishra@catalyst-ed.org>
2025-12-26 20:48:57 +0530

    add colors combinator and switch button in profile

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -2,62 +2,68 @@ import { Tabs } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useThemeStore } from "@/store/useThemeStore";
+import { View } from "react-native";
 
 export default function TabsLayout() {
   const { vars, mode } = useThemeStore();
 
   return (
-    <Tabs
+    <View
       key={mode}
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: vars["--surface"],
-          borderTopColor: vars["--hover"],
-        },
-        tabBarActiveTintColor: vars["--text"],
-        tabBarInactiveTintColor: vars["--text-light"],
-      }}
+      style={vars}
+      className="flex-1 bg-bg"
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="home" color={color} size={size} />
-          ),
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: vars["--surface"],
+            borderTopColor: vars["--hover"],
+          },
+          tabBarActiveTintColor: vars["--text"],
+          tabBarInactiveTintColor: vars["--text-light"],
         }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="home" color={color} size={size} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="exercises"
-        options={{
-          title: "Exercises",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="barschart" color={color} size={size} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="exercises"
+          options={{
+            title: "Exercises",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="barschart" color={color} size={size} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="workout"
-        options={{
-          title: "Workout",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="fitness-center" color={color} size={size} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="workout"
+          options={{
+            title: "Workout",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="fitness-center" color={color} size={size} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="user" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="user" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
